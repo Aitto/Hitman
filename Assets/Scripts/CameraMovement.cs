b9515f45669a0dbc7a9a4f9205e29756bf3d31bf
Aditya <adityachakma199@gmail.com>
2020-03-03 21:47:56 +0600

    Player movement, Camera movement added. Player jump. Floor, wall tagged.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField][Range(1,100)]
+    private float m_Sensitivity = 10.0f;
+    private float yaw,pitch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        yaw = 0.0f;
+        pitch = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX= Input.GetAxis("Mouse X");
+        float mouseY= Input.GetAxis("Mouse Y");
+
+        yaw += mouseX*m_Sensitivity*Time.deltaTime;
+        pitch -= mouseY*m_Sensitivity*Time.deltaTime;
+
+        transform.eulerAngles = new Vector3(pitch,yaw,0.0f);
+
+    }
+
+    public Vector3 GetYRot()
+    {
+        return new Vector3(0.0f,yaw,0.0f);
+        //return Vector3.zero;
+    }
+}
+
+/*
+using UnityEngine;
+ 
+[AddComponentMenu("Camera/Simple Smooth Mouse Look ")]
+public class CameraMovement : MonoBehaviour
+{
+    Vector2 _mouseAbsolute;
+    Vector2 _smoothMouse;
+ 
+    public Vector2 clampInDegrees = new Vector2(360, 180);
+    public bool lockCursor;
+    public Vector2 sensitivity = new Vector2(2, 2);
+    public Vector2 smoothing = new Vector2(3, 3);
+    public Vector2 targetDirection;
+    public Vector2 targetCharacterDirection;
+ 
+    // Assign this if there's a parent object controlling motion, such as a Character Controller.
+    // Yaw rotation will affect this object instead of the camera if set.
+    public GameObject characterBody;
+ 
+    void Start()
+    {
+        // Set target direction to the camera's initial orientation.
+        targetDirection = transform.localRotation.eulerAngles;
+ 
+        // Set target direction for the character body to its inital state.
+        if (characterBody)
+            targetCharacterDirection = characterBody.transform.localRotation.eulerAngles;
+    }
+ 
+    void Update()
+    {
+        // Ensure the cursor is always locked when set
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+ 
+        // Allow the script to clamp based on a desired target value.
+        var targetOrientation = Quaternion.Euler(targetDirection);
+        var targetCharacterOrientation = Quaternion.Euler(targetCharacterDirection);
+ 
+        // Get raw mouse input for a cleaner reading on more sensitive mice.
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+ 
+        // Scale input against the sensitivity setting and multiply that against the smoothing value.
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
+ 
+        // Interpolate mouse movement over time to apply smoothing delta.
+        _smoothMouse.x = Mathf.Lerp(_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
+        _smoothMouse.y = Mathf.Lerp(_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
+ 
+        // Find the absolute mouse movement value from point zero.
+        _mouseAbsolute += _smoothMouse;
+ 
+        // Clamp and apply the local x value first, so as not to be affected by world transforms.
+        if (clampInDegrees.x < 360)
+            _mouseAbsolute.x = Mathf.Clamp(_mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
+ 
+        // Then clamp and apply the global y value.
+        if (clampInDegrees.y < 360)
+            _mouseAbsolute.y = Mathf.Clamp(_mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
+ 
+        transform.localRotation = Quaternion.AngleAxis(-_mouseAbsolute.y, targetOrientation * Vector3.right) * targetOrientation;
+ 
+        // If there's a character body that acts as a parent to the camera
+        if (characterBody)
+        {
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, Vector3.up);
+            characterBody.transform.localRotation = yRotation * targetCharacterOrientation;
+        }
+        else
+        {
+            var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, transform.InverseTransformDirection(Vector3.up));
+            transform.localRotation *= yRotation;
+        }
+    }
+
+    public Vector3 GetYRot()
+    {
+        //return new Vector3(0.0f,yaw,0.0f);
+        return Vector3.zero;
+    }
+}*/
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float m_speed = 5.0f,m_jumpForce = 10.0f;
+    private Vector3 m_forward,m_right,m_up;
+    // Start is called before the first frame update
+    [SerializeField]
+    private CameraMovement m_cm;
+    private Vector3 m_lastPos,m_newPos;
+    private bool m_onTheFloor;
+
+    private Rigidbody m_rigidBody;
+
+    
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+    void Start()
+    {
+        if(Application.targetFrameRate!=60)
+            Application.targetFrameRate = 60;
+        m_onTheFloor = false;
+
+        m_rigidBody = this.gameObject.GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_forward = this.transform.forward; //z axis
+        m_right = this.transform.right;
+        m_up = this.transform.up;
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        //If not on the floor, then can't move the body, but can rotate with the mouse view
+        if(!m_onTheFloor)
+        {
+            this.transform.eulerAngles = m_cm.GetYRot();
+            return;
+        }
+
+        if(Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) )
+        {
+            this.transform.position = this.transform.position + m_forward*m_speed*Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+        {
+            this.transform.position = this.transform.position - m_forward*m_speed*Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            this.transform.position = this.transform.position - m_right*m_speed*Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            this.transform.position = this.transform.position + m_right*m_speed*Time.deltaTime;
+        }
+
+        float jumpAxis = Input.GetAxis("Jump");
+        m_rigidBody.AddForce(m_up*jumpAxis*Time.deltaTime*m_jumpForce);
+
+        //Rotate body along with view
+        this.transform.eulerAngles = m_cm.GetYRot();
+        
+    }
+
+    private void OnCollisionStay(Collision other) {
+        
+        m_lastPos = this.transform.position;
+
+        //Check if the rigidbody is touching the floor
+        if( other.gameObject.tag == "Floor")
+        {
+            m_onTheFloor = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other) {
+
+        //Check if the rigidbody stopped touching the floor
+        if(other.gameObject.tag == "Floor")
+        {
+            m_onTheFloor = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision other) {
+        m_newPos = this.transform.position;
+        Debug.Log("Jump Distance: " + Vector3.Distance(m_newPos,m_lastPos) );
+    }
+
+}
